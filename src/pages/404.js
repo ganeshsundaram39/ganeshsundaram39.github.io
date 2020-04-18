bc24fdb073431361b4171a3bc1d551924e23fe2e
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2020-04-18 23:08:07 +0530

    feat: 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,14 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 style={{
+      color: 'black',
+      margin: '20vh 0 0',
+      marginLeft: '5vw',
+    }}>NOT FOUND</h1>
+    <p style={{
+      marginLeft: '5vw'
+    }}>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
