6facce16025ee16532d125c3af83dd375ee458a3
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2020-04-26 13:47:09 +0530

    fix: svg icons enormous size on load fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,15 @@ import Intro from "../components/intro/intro"
 import WorkSamples from "../components/work-samples/work-samples"
 import Contact from "../components/contact/contact"
 import Footer from "../components/footer/footer"
-
+import { createGlobalStyle } from "styled-components";
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+const GlobalStyles = createGlobalStyle`
+    ${dom.css()}
+`;
 const IndexPage = () => (
   <Layout title="Front-End Web Developer">
+    <GlobalStyles />
     <Header></Header>
     <Intro></Intro>
     <WorkSamples></WorkSamples>
